e478150d0d1c1f978a0eb856785dbf3ce397da27
mm4096 <minecraftmath4096@gmail.com>
2024-09-11 09:52:54 +1200

    build.sh changes

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # activate venv
 source .venv/bin/activate
 
-pyinstaller --onefile main.py
+pyinstaller --onefile main.py --hidden-import mido.backends.rtmidi
 mv -f dist/main dist/MIDI-Controller-linux
