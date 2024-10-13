1458351a3fb41670905e905101f86145baf3c3be
mm4096 <minecraftmath4096@gmail.com>
2024-10-13 19:47:10 +1300

    fixed missing `textual.widgets._tabbed_pane` import

--- build_textual.sh
+++ build_textual.sh
@@ -2,7 +2,7 @@
 source .venv/bin/activate
 
 echo Building EXE
-pyinstaller -F textual_main.py --add-data css:css --hidden-import mido.backends.rtmidi
+pyinstaller -F textual_main.py --add-data css:css --hidden-import mido.backends.rtmidi --hidden-import textual.widgets._tab_pane
 echo Copying to \"dist/MIDI-Controller-textual-linux\"
 mv -f dist/textual_main dist/MIDI-Controller-textual-linux
 
