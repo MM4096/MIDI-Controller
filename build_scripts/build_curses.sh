f5680d4b6cb17ea8efa6e33d959016bb168aec4a
mm4096 <minecraftmath4096@gmail.com>
2024-11-13 20:00:33 +1300

    changed build program to open a console window

--- build_scripts/build_curses.sh
+++ build_scripts/build_curses.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd ..
 # activate venv
 source .venv/bin/activate
 
-pyinstaller --onefile curses_main.py --hidden-import mido.backends.rtmidi
+pyinstaller --console --onefile curses_main.py --hidden-import mido.backends.rtmidi
 mv -f dist/curses_main dist/MIDI-Controller-curses-linux
--- build_scripts/build_textual.sh
+++ build_scripts/build_textual.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd ..
 source .venv/bin/activate
 
 echo Building EXE
-pyinstaller -F textual_main.py --add-data css:css --hidden-import mido.backends.rtmidi --hidden-import textual.widgets._tab_pane
+pyinstaller --console -F textual_main.py --add-data css:css --hidden-import mido.backends.rtmidi --hidden-import textual.widgets._tab_pane
 echo Copying to \"dist/MIDI-Controller-textual-linux\"
 mv -f dist/textual_main dist/MIDI-Controller-textual-linux
 
